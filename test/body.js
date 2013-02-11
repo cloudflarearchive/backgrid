8c2e31b2003b1c6ec3fdd37cb25f56c0c543acb8
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2013-02-11 20:59:29 +0800

    Test case for #44 and #45

--- test/body.js
+++ test/body.js
@@ -155,4 +155,43 @@ describe("A Body", function () {
     expect(CustomRow.prototype.render).toHaveBeenCalled();
   });
 
+  describe("maintain page size at page boundary", function () {
+
+    var col;
+
+    beforeEach(function () {
+      col = new Backbone.PageableCollection([
+        {id: 1},
+        {id: 2},
+        {id: 3}
+      ], {
+        state: {
+          pageSize: 2
+        },
+        mode: "client"
+      });
+
+      body = new Backgrid.Body({
+        columns: [{
+          name: "id",
+          cell: "integer"
+        }],
+        collection: col
+      });
+
+      body.render();
+    });
+
+    it("when adding to a full page", function () {
+      col.add(new Backbone.Model({id: 4}));
+      expect(body.$el.find("tr").length).toBe(2);
+    });
+
+    it("when removing from a full page", function () {
+      col.remove(col.get(1));
+      expect(body.$el.find("tr").length).toBe(2);
+    });
+
+  });
+
 });
