14c7ecdcec61bc55b13aeb22476c6d9b8fa97fa0
Paolo Ardoino <paolo.ardoino@gmail.com>
2013-08-25 11:17:29 +0200

    Fixed Cell rendering
    Model was not correctly passed as arugment of the formatter

--- src/cell.js
+++ src/cell.js
@@ -260,7 +260,7 @@ var Cell = Backgrid.Cell = Backbone.View.extend({
   render: function () {
     this.$el.empty();
     var model = this.model;
-    this.$el.text(this.formatter.fromRaw(model.get(this.column.get("name"))), model);
+    this.$el.text(this.formatter.fromRaw(model.get(this.column.get("name")), model));
     this.delegateEvents();
     return this;
   },
