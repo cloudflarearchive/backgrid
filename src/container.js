5c03d6b3c59fe6bff2542c103e42acfd7eb23edb
Bruno Carriere <neonskimmer@gmail.com>
2014-01-01 12:02:06 -0500

    Added prefetching

--- src/container.js
+++ src/container.js
@@ -28,6 +28,7 @@ var Container = Backgrid.Container = Backbone.View.extend({
 
     this.height = options.height || 200;
     this.throttle = options.throttle || this.throttle;
+    this.prefetch = "prefetch" in options ? options.prefetch : this.prefetch;
     this.grid = options.grid;
     this.onScroll = _.debounce(_.bind(this.onScroll, this), this.throttle);
     this.indicator = $("<h1>HEY I'M LOADING OVER HERE</h1>").hide();
@@ -37,25 +38,44 @@ var Container = Backgrid.Container = Backbone.View.extend({
 
   onScroll: function (e) {
     var top = e.target.scrollTop,
-        prefetch = this.prefetch,
-        maxScrollTop = e.target.scrollHeight - this.$el.height(),
+        shouldFetch = top >= e.target.scrollHeight - this.$el.height(),
         collection = this.grid.collection.pageableCollection,
-        indicator = this.indicator,
+        hideIndicator = _.bind(this.hideIndicator, this),
+        prefetch = this.prefetch,
         next;
 
-    if (top >= maxScrollTop) {
-      if (next = collection.getNextPage({
-        scrolling: true
-      })) {
+    if (shouldFetch) {
+      if (next = collection.getNextPage({ scrolling: true })) {
         next.then(function() {
-          prefetch && collection.hasNext() && collection.getNextPage({
-            scrolling: true
-          });
-        }).always(function() {
-          indicator.hide();
-        });
+          if (!prefetch) return;
+
+          // Prefetching sequence
+          (function prefetchNext(remaining) {
+            if (!remaining) return;
+
+            console.info("prefetching, remaining ", remaining);
+
+            if (collection.hasNext()) {
+              collection.getNextPage({
+                scrolling: true
+              }).then(function() {
+                console.info("prefetched ", remaining, "decreasing remaining");
+                return --remaining;
+              }).then(prefetchNext).always(hideIndicator);
+            }
+          })(prefetch);
+
+        }).always(hideIndicator);
       }
     }
+  },
+
+  showIndicator: function() {
+    this.indicator.detach().appendTo(this.grid.footer.el);
+  },
+
+  hideIndicator: function() {
+    this.indicator.hide();
   },
 
   onRequest: function (collection, xhr, options) {
@@ -67,7 +87,8 @@ var Container = Backgrid.Container = Backbone.View.extend({
       height: this.height + "px",
       overflow: "scroll"
     }));
-    this.indicator.detach().appendTo(this.grid.footer.el);
+
+    this.hideIndicator();
     this.delegateEvents();
     return this;
   }
