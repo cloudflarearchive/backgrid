3aef8d732d3d04c6d1c990668012373e3f5e479c
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2016-04-11 01:32:27 +0100

    Turn watch on by default

--- karma.conf.js
+++ karma.conf.js
@@ -67,7 +67,7 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
 
     // start these browsers
